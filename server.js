9a918b6bbfec32b5fd7d07ec95532e386b85a60f
Ayushman <ayushmanraghav.ra@gmail.com>
2021-07-03 12:53:17 +0530

    unix timestamp to date

--- server.js
+++ server.js
@@ -2,50 +2,90 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
 app.get("/api/:date", function (req, res) {
-  const  date = new Date(req.params.date);
-  const utc = getUtcTime(date);
-  res.json({
-    unix: Math.round( date.getTime()),
-    utc: utc
-  });
+  let dateString = req.params.date;
+  let date = null;
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    date = new Date(dateInt);
+    // Date regards numbers as unix timestamps, strings are processed differently
+    // res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  } else {
+     date = new Date(dateString);
+    }
+    
+  if (date.toString() === "invalid date") res.json({ error: "invalid date" });
+  else {
+    const utc = getUtcTime(date);
+    res.json({
+      unix: Math.round(date.getTime()),
+      utc: utc,
+    });
+  }
+  // const  date = new Date(parseInt(req.params.date));
 });
 
-function getUtcTime(date) { 
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+function getUtcTime(date) {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   const h = addZero(date.getUTCHours(), 2);
   const m = addZero(date.getUTCMinutes(), 2);
   const s = addZero(date.getUTCSeconds(), 2);
-  
-  const utc = days[date.getUTCDay()]+", "+ date.getUTCDate() + " " + months[date.getUTCMonth()] + " " + date.getUTCFullYear() + " " + h + ':' + m + ':' + s + ' GMT';
+
+  const utc =
+    days[date.getUTCDay()] +
+    ", " +
+    date.getUTCDate() +
+    " " +
+    months[date.getUTCMonth()] +
+    " " +
+    date.getUTCFullYear() +
+    " " +
+    h +
+    ":" +
+    m +
+    ":" +
+    s +
+    " GMT";
 
   return utc;
 }
 
-function addZero(x,n) {
+function addZero(x, n) {
   while (x.toString().length < n) {
     x = "0" + x;
   }
@@ -54,5 +94,5 @@ function addZero(x,n) {
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
